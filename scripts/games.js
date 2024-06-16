dfca379db014ac1c3d8277391e8509f08906f525
Your Name <you@example.com>
2024-06-16 17:27:44 +0300

    style

--- scripts/games.js
+++ scripts/games.js
@@ -1,8 +1,25 @@
 $(document).ready(async function () {
-    let response = await getAllGames()
 
-    let games = await response.json()
+    let games = await getAllGames()
+
+    games.forEach(e => {
+
+        $(".games-list").append(`
+      <div class="game-card">
+                <img src="${e.titleImageUrl}">
+                <h2> ${e.title} </h2>
+                <p> ${e.price} </p>
+                <a href="/pes-patron-fans-project/pages/game-page.html?id=${e.id}"> <button> Придбати </button> </a>
+      </div>
+    `)
+    })
+    games =  games.sort((e1, e2, e3) => e1.averageRating - e2.averageRating - e3.averageRating)
+
     console.log(games)
-
 });
+
+function addgame() {
+    games.append(games.id + games.title + games.price + games.titleImageUrl);
+}
+
 
