6ff7275d5d0b5f1229e89766313b94dc43019860
igorshkurko <129660003+igorshkurko@users.noreply.github.com>
2024-06-22 12:36:29 +0300

    Змінив головну сторінку

--- scripts/games.js
+++ scripts/games.js
@@ -1,13 +1,30 @@
-$(document).ready(async function () {
+let a = 0
 
+
+$(document).ready(async function () {
+    $(".login").hide();
+
+
+    $('#login2').click(function (e) { 
+        if (a % 2 == 0) {
+            $(".login").show();
+            a++
+        } else {
+            $(".login").hide();
+            a++
+        }
+       
+        
+    });
+    
     let games = await getAllGames()
 
     games.forEach(e => {
 
         $(".games-list").append(`
       <div class="game-card">
-                <img src="${e.titleImageUrl}">
-                <h2> ${e.title} </h2>
+                <img class="icon" src="${e.titleImageUrl}">
+                <h2 class="name"> ${e.title} </h2>
                 <p> ${e.price} </p>
                 <a href="/pes-patron-fans-project/pages/game-page.html?id=${e.id}"> <button> Придбати </button> </a>
       </div>
@@ -16,6 +33,8 @@ $(document).ready(async function () {
     games =  games.sort((e1, e2, e3) => e1.averageRating - e2.averageRating - e3.averageRating)
 
     console.log(games)
+
+
 });
 
 function addgame() {
